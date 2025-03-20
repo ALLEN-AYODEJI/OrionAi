9de7f9fa64545fc641bde78f5b42a1339ddf54f6
ALLEN-AYODEJI <allenzeereeh@gmail.com>
2025-03-20 13:58:39 +0100

    Worked on clerk for user authentication

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,8 +1,15 @@
 import Image from 'next/image'
 import { assets } from '@/assets/assets'
 import React from 'react'
+import { useClerk, UserButton } from '@clerk/nextjs'
+import { useAppContext } from '@/context/AppContext'
 
 const Sidebar = ({expand, setExpand}) => {
+
+  const{openSignIn} = useClerk()
+  const {user} = useAppContext()
+
+
   return (
     <div className={`flex flex-col justify-between bg-black	 pt-7 transition-all
     z-50 max-md:absolute max-md:h-screen ${expand ? 'p-4 w-64' : 'md:w-20 w-0 max-md:overflow-hidden'}`}>
@@ -61,9 +68,13 @@ const Sidebar = ({expand, setExpand}) => {
           {expand && <><span>Get App</span> <Image src={assets.new_icon}/></>}
         </div>
 
-        <div className={`flex items-center ${expand ? 'hover:bg-white/10 rounded-lg' : 'justify-center w-full'}
+        <div onClick={user ? null : openSignIn  }
+        className={`flex items-center ${expand ? 'hover:bg-white/10 rounded-lg' : 'justify-center w-full'}
         gap-3 text-white text-sm p-2 mt-2 cursor-pointer`}>
-          <Image src={assets.profile_icon} alt='' className='w-7'/>
+          {
+            user ? <UserButton/>
+            : <Image src={assets.profile_icon} alt='' className='w-7'/>
+          }
           {expand && <span>My Profile</span>}
         </div>
       </div>
