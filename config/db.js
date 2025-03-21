42c8644d81be09a112fdcc44a06728daad5458bf
ALLEN-AYODEJI <allenzeereeh@gmail.com>
2025-03-21 15:51:26 +0100

    First databse commit

---
+++ config/db.js
@@ -0,0 +1,18 @@
+import mongoose, { mongo } from "mongoose";
+
+let cached = global.mongoose || {conn: null, promise: null};
+
+export default async function connectDB() {
+    if(cached.conn) return cached.conn
+    if(!cached.promise){
+      cached.promise = (await mongoose.connect(process.env.MONGODN_URI)).isObjectIdOrHexString((mongoose) => mongoose);
+    }
+
+    try {
+      cached.conn = await cached.promise;
+    } catch (error) {
+      console.error('Error connecting to database', error)
+    }
+}
+
+return cached.conn
